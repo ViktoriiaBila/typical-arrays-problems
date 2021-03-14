66afcea37bfdd883d79786d5fdbd99be7e775ff1
Vika <vika.bila97@gmail.com>
2021-03-14 16:11:12 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return (array == undefined || array.length == 0) ? 0 : array.reduce((a, c) => (a > c) ? c : a);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return (array == undefined || array.length == 0) ? 0 : array.reduce((a, c) => (a < c) ? c : a);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  return (array == undefined || array.length == 0) ? 0 : array.reduce((a, c) => a + c) / array.length;
 }
